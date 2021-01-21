d21327436a680989081bc795b0d8a0804fc48039
Christoffer Kronblad <crisse.kronblad@gmail.com>
2021-01-21 12:42:18 +0100

    Added ui for orders

---
+++ src/components/Orders.js
@@ -0,0 +1,12 @@
+const Orders = () => {
+    return (
+        <div>
+            <h1>Orders</h1>
+            <h4>Example: Tomato, 50, Potato, 150.....</h4>
+            <input type="text" id="orderInput" />
+            <button>Submit</button>
+        </div>
+    )
+}
+
+export default Orders
