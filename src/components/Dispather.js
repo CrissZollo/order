019d2602f1d961b951169ed13f5d774520edae39
Christoffer Kronblad <crisse.kronblad@gmail.com>
2021-02-04 14:57:15 +0100

    Start on react

--- src/components/Dispather.js
+++ src/components/Dispather.js
@@ -9,96 +9,9 @@ const Dispather = ({ orderArr }) => {
     let [stopValue, setStopValue] = useState(0)
 
 
-    class Node {
-        constructor(data, next = null) {
-            this.data = data;
-            this.next = next;
-        }
-    }
-
-    class Fifo {
-        constructor() {
-            this.head = null;
-            this.size = 0;
-        }
-
-        // Numbers of jobs left
-        numberOfJobsInQueue() {
-            return this.size;
-        }
-
-        //Insert last Node
-        addJob(data) {
-            let node = new Node(data);
-            let current;
-
-            // If empty, make head
-            if (this.head == null) {
-                this.head = node;
-
-            } else {
-                current = this.head;
-
-                while (current.next != null) {
-                    current = current.next;
-                }
-
-                current.next = node;
-            }
-
-            this.size++;
-        }
-
-        //Remove at Index
-        removeJob(index) {
-            if ((index > 0 && index > this.size) || index < 0) {
-                console.error("Index Out Of Range");
-                return;
-            }
-
-            let current = this.head
-            let previous;
-            let count = 0;
-
-            //Remove First
-            if (index === 0) {
-                this.head = current.next
-            } else {
-                while (count < index) {
-                    count++;
-                    previous = current
-                    current = current.next;
-                }
-
-                previous.next = current.next
-            }
-
-            this.size--;
-
-        }
-
-        //Print list data
-        listJobs() {
-            let current = this.head;
-            let listArr = [];
-
-            while (current != null) {
-                listArr.push(current.data);
-                current = current.next;
-            }
-
-            return listArr;
-        }
-    }
-
-    const fifoList = new Fifo();
-
-
 
 
     //const [fifoList, setFifoList] = useState(new Fifo());
-
-    let [time, setTime] = useState(0);
 
 
     function useTimer() {
@@ -110,7 +23,6 @@ const Dispather = ({ orderArr }) => {
                 clearInterval(timer)
                 return;
             }
-            setTime(time);
             fifoList.addJob(orderArr[orderArr.length - (time + 1)]);
             orderArr.pop();
             time++;
